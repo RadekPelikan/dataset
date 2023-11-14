f7ccd0a8e849913d9621a139a6369bc34a362ce1
Radek Pelikan (EIT) <radek.pelikan@embedit.com>
2023-11-14 09:20:59 +0100

    box values set rgb

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,19 @@
 const boxes = document.getElementsByClassName("box");
 
+const color = {
+  red: 0,
+  green: 0,
+  blue: 0,
+}
+
 window.onload = () => {
-  [...boxes].forEach((box) => {
-    box.onclick = () => {
-      document.body.style.backgroundColor = box.dataset.color;
-      box.innerText++;
-    };
-  });
+  [...boxes].forEach((box) => box.onclick = handleClick);
 };
+
+const handleClick = (e) => {
+  const box = e.target
+  console.log({color})
+  color[box.dataset.color] = (color[box.dataset.color] + 1) % 256;
+  document.body.style.backgroundColor = `rgb(${color.red} ${color.green} ${color.blue})`
+  box.innerText = color[box.dataset.color]
+}
